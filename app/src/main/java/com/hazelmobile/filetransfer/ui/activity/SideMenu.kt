fec5ccbf3c1f2d4ac456b94588dff0017c45c239
faizanalibutt <fayzanalibutt@gmail.com>
2019-08-28 18:39:38 +0500

    implementing ui....Menu Screen iteration undergoing.

--- app/src/main/java/com/hazelmobile/filetransfer/ui/activity/SideMenu.kt
+++ app/src/main/java/com/hazelmobile/filetransfer/ui/activity/SideMenu.kt
@@ -1,6 +1,8 @@
 package com.hazelmobile.filetransfer.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import com.hazelmobile.filetransfer.R
 
 class SideMenu : BaseActivity() {
@@ -8,5 +10,13 @@ class SideMenu : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_side_menu)
+    }
+
+    fun openSettings(view: View) {
+        startActivity(Intent(this@SideMenu, SettingsActivity::class.java))
+    }
+
+    fun closeMenu(view: View) {
+        finish()
     }
 }
