3693f988c8526f0ac5b12738e9aa91b0b9209732
faizanalibutt <fayzanalibutt@gmail.com>
2019-08-30 18:25:08 +0500

    implementing ui....Making Settings Screen UI under process.

--- app/src/main/java/com/hazelmobile/filetransfer/ui/activity/SettingsActivity.kt
+++ app/src/main/java/com/hazelmobile/filetransfer/ui/activity/SettingsActivity.kt
@@ -8,7 +8,7 @@ class SettingsActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
     }
 
