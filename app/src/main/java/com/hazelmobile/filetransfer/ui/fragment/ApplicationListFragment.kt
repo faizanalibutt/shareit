2f422fdcea5ccd2024555ab10b5925c0b712426a
faizanalibutt <fayzanalibutt@gmail.com>
2019-09-06 18:16:21 +0500

    implementing ui...File Manager implementing...main design is ready.

--- app/src/main/java/com/hazelmobile/filetransfer/ui/fragment/ApplicationListFragment.kt
+++ app/src/main/java/com/hazelmobile/filetransfer/ui/fragment/ApplicationListFragment.kt
@@ -11,22 +11,17 @@ import android.view.ViewGroup
 import com.hazelmobile.filetransfer.R
 import com.hazelmobile.filetransfer.utils.callback.TitleSupport
 
-/**
- * A simple [Fragment] subclass.
- */
 class ApplicationListFragment : Fragment(), TitleSupport {
-
-    override fun getTitle(context: Context): CharSequence {
-        return "Apps"
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_application_list, container, false)
     }
 
+    override fun getTitle(context: Context): CharSequence {
+        return context.getString(R.string.text_application)
+    }
 
 }
