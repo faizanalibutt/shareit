4eae15245cdfffe3661ee53591333541fb567993
faizanalibutt <fayzanalibutt@gmail.com>
2019-09-26 15:03:03 +0500

    implementing ui...File Manager implementing...
    file explorer...implemented looking for icons....
    icons put on....change the order...looking for action callback...

---
+++ app/src/main/java/com/hazelmobile/filetransfer/widget/Extensions.kt
@@ -0,0 +1,8 @@
+@file:JvmName("ExtensionsUtils")
+package com.hazelmobile.filetransfer.widget
+
+import android.util.Log
+
+fun getLogInfo(logInfo: String) {
+    Log.d("file_transfer_debug", logInfo)//ExtensionsUtils.getLogInfo(" ");
+}
