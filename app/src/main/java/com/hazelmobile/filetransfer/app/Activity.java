eaec747525606100c0e3cf6dcf39f8bf53bae5c8
faizanalibutt <fayzanalibutt@gmail.com>
2019-10-25 10:34:07 +0500

    implementing ui...File Transfer implementing...
    Send Activity adding final functionality.

--- app/src/main/java/com/hazelmobile/filetransfer/app/Activity.java
+++ app/src/main/java/com/hazelmobile/filetransfer/app/Activity.java
@@ -35,8 +35,6 @@ import com.hazelmobile.filetransfer.files.AppConfig;
 import com.hazelmobile.filetransfer.pictures.AppUtils;
 import com.hazelmobile.filetransfer.service.CommunicationService;
 import com.hazelmobile.filetransfer.service.WorkerService;
-import com.hazelmobile.filetransfer.ui.activity.WelcomeActivity;
-
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -126,8 +124,8 @@ public abstract class Activity extends AppCompatActivity {
             recreate();
 
         if (!hasIntroductionShown() && !mWelcomePageDisallowed) {
-            startActivity(new Intent(this, WelcomeActivity.class));
-            finish();
+            //startActivity(new Intent(this, WelcomeActivity.class));
+            //finish();
         } else if (!AppUtils.checkRunningConditions(this)) {
             if (!mSkipPermissionRequest)
                 requestRequiredPermissions(true);
