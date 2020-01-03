7bd7d6f6cd0d1fffc24e5ca296d60fdddebea659
faizanalibutt <fayzanalibutt@gmail.com>
2020-01-03 15:49:52 +0500

    implementing ui...File Transfer...
    File Transfer...Connection/Transfer Module
    ...updated bluetooth logic...
    ...look for more updates...

--- app/src/main/java/com/hazelmobile/filetransfer/ui/activity/MainActivity.kt
+++ app/src/main/java/com/hazelmobile/filetransfer/ui/activity/MainActivity.kt
@@ -2,10 +2,8 @@ package com.hazelmobile.filetransfer.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import com.hazelmobile.filetransfer.R
 import com.hazelmobile.filetransfer.app.Activity
-import com.hazelmobile.filetransfer.util.ConnectionUtils
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
@@ -17,10 +15,6 @@ class MainActivity : Activity() {
         slide_menu.setOnClickListener {
             startActivity(Intent(this@MainActivity, SideMenu::class.java))
         }
-
-        Snackbar.make(container, "${ConnectionUtils.getInstance(this).isBleAvailable}",
-            Snackbar.LENGTH_INDEFINITE).show()
-
 
     }
 
